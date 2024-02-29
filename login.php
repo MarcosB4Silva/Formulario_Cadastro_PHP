c88773bfba9ea50bd0d2896571a69660438f3e2b
MarcosB4Silva <marcosbs252@gmail.com>
2024-02-29 14:04:49 -0300

    adicionando tela login/home

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="tela_login.css">
+    <title>Login</title>
+</head>
+<body>
+    <div class="box">
+        <h1> Realize o Login </h1>
+        <form action="testarlogar.php" method="POST">
+            <input type="text" name="email" placeholder="E-mail" class="inputUser">
+            <br><br>
+            <input type="password" name="senha" placeholder="Senha" class="inputUser">
+            <br><br>
+            <input type="submit" name="submit" value="Enviar" class="btn">
+            <br><br>
+        </form>
+    </div>
+</body>
+</html>
---
+++ testarlogar.php
@@ -0,0 +1,45 @@
+<?php
+// Iniciando a sessão
+session_start();
+
+// ! TESTE: Testando retorno da tela.
+// print_r($_REQUEST);
+if(isset($_POST['submit']) && !empty($_POST['email']) && !empty($_POST['senha']))
+{
+    include_once('config.php');
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    // ! TESTE: Testando dados digitados.
+    // print_r('Email: ' . $email);
+    // print_r('<br>');
+    // print_r('Senha: ' . $senha);
+
+    $sql = "SELECT * FROM fomulario_cadastro WHERE email = '$email' and senha = '$senha'";
+
+    $result = $conexao->query($sql);
+
+    // ! TESTE: Testando o funcionamento do select.
+    // print_r($sql);
+    // print_r('<br>');
+    // print_r($result);
+
+    if(mysqli_num_rows($result) < 1)
+    {
+        unset ($_SESSION['email']);
+        unset ($_SESSION['senha']);
+        header('Location: login.php');
+    }
+    else
+    {
+        $_SESSION['email'] = $email;
+        $_SESSION['senha'] = $senha;
+        header('Location: homedosistema.php');
+    }
+}
+else
+{
+    header('Location: login.php');
+}
+
+?>
